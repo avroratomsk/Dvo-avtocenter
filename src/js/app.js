3ad6bec93812fffeba50d16920a14d71b71bc81e
sanchyopas <saniagolovanev@gmail.com>
2025-04-30 16:49:29 +0700

    slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,39 @@
 import * as functions from './modules/baseFunctions.js';
 
 functions.isWebp();
+
+const  swipes_slider = new Swiper('.slider__swiper', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    dynamicBullets: true,
+  },
+});
+
+const catalog_slider = new Swiper('.catalog-slider__swiper', {
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 12,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 24
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 24
+    }
+  }
+});
