4dc516efbecf1f43e3cd36f3dc23e13348d6412f
sanchyopas <saniagolovanev@gmail.com>
2025-04-30 18:06:06 +0700

    sticky scroll

--- src/js/app.js
+++ src/js/app.js
@@ -37,3 +37,24 @@ const catalog_slider = new Swiper('.catalog-slider__swiper', {
     }
   }
 });
+
+const lenis = new Lenis({
+  autoRaf: true,
+});
+
+window.addEventListener('scroll', () => {
+  const menuPosition = window.scrollY;
+  const menuElem = document.querySelector('.header-bottom__inner');
+  const topPositionMenuElem = menuElem.getBoundingClientRect().top;
+  const headerBottomElem = document.querySelector('.header-bottom');
+  const headerBottomElemHeight = headerBottomElem.getBoundingClientRect().height;
+
+
+  if (menuPosition > topPositionMenuElem) {
+    headerBottomElem.classList.add('header--fixed');
+    document.documentElement.style.paddingTop = headerBottomElemHeight + "px"
+  }else {
+    headerBottomElem.classList.remove('header--fixed');
+    document.documentElement.style.paddingTop = "0px"
+  }
+})
