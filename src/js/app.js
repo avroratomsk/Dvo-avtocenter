6e5e46da4fc154f01d78b06dd91b8a8fc76356aa
sanchyopas <saniagolovanev@gmail.com>
2025-05-15 18:00:37 +0700

    dev single page

--- src/js/app.js
+++ src/js/app.js
@@ -1,43 +1,11 @@
 import * as functions from './modules/baseFunctions.js';
 
+// import Swiper from 'swiper/bundle';
+
 functions.isWebp();
 
-const  swipes_slider = new Swiper('.slider__swiper', {
-  loop: true,
-  pagination: {
-    el: '.swiper-pagination',
-    dynamicBullets: true,
-  },
 
-});
 
-const catalog_slider = new Swiper('.catalog-slider__swiper', {
-  loop: true,
-  slidesPerView: 4,
-  spaceBetween: 12,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  // Responsive breakpoints
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 20
-    },
-    // when window width is >= 480px
-    480: {
-      slidesPerView: 3,
-      spaceBetween: 24
-    },
-    // when window width is >= 640px
-    640: {
-      slidesPerView: 4,
-      spaceBetween: 24
-    }
-  }
-});
 
 // const lenis = new Lenis({
 //   autoRaf: true,
